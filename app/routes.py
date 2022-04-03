65941dce16dbca9627cc80d663351e2303335464
Arsenii Hlazov <glazov.arseniy@gmail.com>
2022-04-03 15:38:37 +0300

    lesson2: add templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, world."
+    user = {'username': 'Arsenii'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Lviv!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Arsenii'},
+            'body': 'It works!'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
